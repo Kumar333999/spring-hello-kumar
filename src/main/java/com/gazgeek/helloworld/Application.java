c72f97843daa3351bc8f6333dabdd62105a34542
Kumar333999 <159309060+Kumar333999@users.noreply.github.com>
2025-11-21 12:32:43 +0530

    Update Application.java

--- src/main/java/com/gazgeek/helloworld/Application.java
+++ src/main/java/com/gazgeek/helloworld/Application.java
@@ -11,6 +11,7 @@ public class Application {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
+        System.out.print(0);
     }
 
 
